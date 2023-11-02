e19c9408aacec9ef82e6138c8ab5db636af7a677
Koby <kobysac@gmail.com>
2023-11-01 23:59:06 -0700

    started building queriesController, getting weird bug when launching app

--- controllers/queriesController.js
+++ controllers/queriesController.js
@@ -1,3 +1,118 @@
 const connection = require('../config/connection');
 const queries = require('../queries');
 
+async function viewDepartments() {
+    
+    console.log("viewing departments");
+};
+
+
+async function viewRoles() {
+
+    console.log("viewing roles");
+};
+
+
+async function viewEmployees() {
+
+    console.log("viewing employees");
+};
+
+
+async function viewSalaries() {
+
+    console.log("viewing salaries");
+};
+
+
+async function addDepartment() {
+
+    console.log("adding departments");
+};
+
+
+async function addRole() {
+
+    console.log("adding roles");
+};
+
+
+async function addEmployee() {
+
+    console.log("adding employee");
+};
+
+
+async function updateDepartment() {
+
+    console.log("updating departments");
+};
+
+
+async function updateRole() {
+
+    console.log("updating roles");
+};
+
+
+async function deleteDepartment() {
+
+    console.log("deleting departments");
+};
+
+
+async function deleteRole() {
+
+    console.log("deleting role");
+};
+
+
+async function deleteEmployee() {
+
+    console.log("deleting employee");
+};
+
+
+async function viewByDept() {
+
+    console.log("viewing employees by department");
+};
+
+
+async function viewByManager() {
+
+    console.log("viewing employees by manager");
+};
+
+
+async function updateEmpRole() {
+
+    console.log("updating employee's role");
+};
+
+
+async function updateEmpManager() {
+
+    console.log("updating employee's manager");
+};
+
+
+
+module.exports = {
+    viewDepartments,
+    viewRoles,
+    viewEmployees,
+    viewSalaries,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateDepartment,
+    updateRole,
+    deleteDepartment,
+    deleteRole,
+    deleteEmployee,
+    viewByDept,
+    viewByManager,
+    updateEmpRole,
+    updateEmpManager,
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const { intro, second, third } = require('./questions');
+const queryDriver = require('./controllers/queriesController.js');
 
 async function askMore(response) {
     console.log(Object.keys(response)[0], response[`${Object.keys(response)[0]}`]);
@@ -8,118 +9,128 @@ async function askMore(response) {
     const value = response[`${Object.keys(response)[0]}`];
 
     let newResponse;
-
-    switch (key) {
-        case 'intro':
-            switch (value) {
-                case 'View':
-                    newResponse = await inquirer.prompt(second.view);
-                    await askMore(newResponse);
-                    break;
-                case 'Add':
-                    newResponse = await inquirer.prompt(second.add);
-                    await askMore(newResponse);
-                    break;
-                case 'Update':
-                    newResponse = await inquirer.prompt(second.update);
-                    await askMore(newResponse);
-                    break;
-                case 'Delete':
-                    newResponse = await inquirer.prompt(second.delete);
-                    await askMore(newResponse);
-                    break;
-                default:
-                    console.log("intro key case error");
-            };
-            break;
-        case 'view':
-            switch (value) {
-                case 'Departments':
-                    console.log("viewing departments");
-                    break;
-                case 'Roles':
-                    console.log("viewing roles");
-                    break;
-                case 'Employees':
-                    newResponse = await inquirer.prompt(third.view_emp);
-                    await askMore(newResponse);
-                    break;
-                case 'Salaries':
-                    console.log("viewing salaries");
-                    break;
-            };
-            break;
-        case 'add':
-            switch (value) {
-                case 'Departments':
-                    console.log("adding departments");
-                    break;
-                case 'Roles':
-                    console.log("adding roles");
-                    break;
-                case 'Employees':
-                    console.log("adding employee");
-                    break;
-            };
-            break;
-        case 'update':
-            switch (value) {
-                case 'Departments':
-                    console.log("updating departments");
-                    break;
-                case 'Roles':
-                    console.log("updating roles");
-                    break;
-                case 'Employees':
-                    newResponse = await inquirer.prompt(third.update_emp);
-                    await askMore(newResponse);
-                    break;
-            };
-            break;
-        case 'delete':
-            switch (value) {
-                case 'Departments':
-                    console.log("deleting departments");
-                    break;
-                case 'Roles':
-                    console.log("deleting roles");
-                    break;
-                case 'Employees':
-                    console.log("deleting employee");
-                    break;
-            };
-            break;
-        case 'view-sort':
-            switch (value) {
-                case 'Department':
-                    console.log("viewing employees by department");
-                    break;
-                case 'Manager':
-                    console.log("viewing employees by manager");
-                    break;
-            };
-            break;
-        case 'update-emp':
-            switch (value) {
-                case 'Role':
-                    console.log("updating employee's role");
-                    break;
-                case 'Manager':
-                    console.log("updating employee's manager");
-                    break;
-            };
-            break;
-    };
+    try {
+        switch (key) {
+            case 'intro':
+                switch (value) {
+                    case 'View':
+                        newResponse = await inquirer.prompt(second.view);
+                        await askMore(newResponse);
+                        break;
+                    case 'Add':
+                        newResponse = await inquirer.prompt(second.add);
+                        await askMore(newResponse);
+                        break;
+                    case 'Update':
+                        newResponse = await inquirer.prompt(second.update);
+                        await askMore(newResponse);
+                        break;
+                    case 'Delete':
+                        newResponse = await inquirer.prompt(second.delete);
+                        await askMore(newResponse);
+                        break;
+                    default:
+                        console.log("intro key case error");
+                };
+                break;
+            case 'view':
+                switch (value) {
+                    case 'Departments':
+                        await queryDriver.viewDepartments();
+                        break;
+                    case 'Roles':
+                        await queryDriver.viewRoles();
+                        break;
+                    case 'Employees':
+                        newResponse = await inquirer.prompt(third.view_emp);
+                        await askMore(newResponse);
+                        break;
+                    case 'Salaries':
+                        await queryDriver.viewSalaries();
+                        break;
+                };
+                break;
+            case 'add':
+                switch (value) {
+                    case 'Departments':
+                        await queryDriver.addDepartment();
+                        break;
+                    case 'Roles':
+                        await queryDriver.addRole();
+                        break;
+                    case 'Employees':
+                        await queryDriver.addEmployee();
+                        break;
+                };
+                break;
+            case 'update':
+                switch (value) {
+                    case 'Departments':
+                        await queryDriver.updateDepartment();
+                        break;
+                    case 'Roles':
+                        await queryDriver.updateRole();
+                        break;
+                    case 'Employees':
+                        newResponse = await inquirer.prompt(third.update_emp);
+                        await askMore(newResponse);
+                        break;
+                };
+                break;
+            case 'delete':
+                switch (value) {
+                    case 'Departments':
+                        await queryDriver.deleteDepartment();
+                        break;
+                    case 'Roles':
+                        await queryDriver.deleteRole();
+                        break;
+                    case 'Employees':
+                        await queryDriver.deleteEmployee();
+                        break;
+                };
+                break;
+            case 'view-sort':
+                switch (value) {
+                    case 'Department':
+                        await queryDriver.viewByDept();
+                        break;
+                    case 'Manager':
+                        await queryDriver.viewByManager();
+                        break;
+                    default:
+                        await queryDriver.viewEmployees();
+                        break;
+                };
+                break;
+            case 'update-emp':
+                switch (value) {
+                    case 'Role':
+                        await queryDriver.updateEmpRole();
+                        break;
+                    case 'Manager':
+                        await queryDriver.updateEmpManager();
+                        break;
+                };
+                break;
+        };
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error asking next question");
+    }
     // console.log("exitting askMore");
-    
+
 }
 
 
 async function main() {
     let runFlag = true;
+    let response;
     while (runFlag) {
         try {
-            const response = await inquirer.prompt(intro);
+            console.log("test1");
+            response = await inquirer.prompt(intro);
+            console.log("test2");
             if (response.intro == 'Quit') {
                 runFlag = false;
             } else {
